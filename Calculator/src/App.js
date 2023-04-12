a3cd81299815311c57ca14f2080508258a538dd7
Mayuri Narute <66169874+MayuriNarute@users.noreply.github.com>
2023-04-12 22:37:32 +0530

    Add files via upload

---
+++ Calculator/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Calci from './components/calci';
+import Navbar from './components/Navbar';
+function App() {
+
+  return (
+    
+  <>
+    
+    <Navbar/>
+    <Calci/>  
+    </>
+
+  );
+}
+
+export default App;
---
+++ Calculator/src/components/Navbar.js
@@ -0,0 +1,25 @@
+
+function Navbar() {
+
+  return (
+    <>
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">React Calculator</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        
+        
+      </div>
+    </div>
+  </nav>
+  <br></br>
+  <br></br>
+
+  </>
+  );
+}
+
+export default Navbar;
